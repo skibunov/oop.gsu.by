981b34e3a88f9d2453a7cfa291a5417e9f5981dd
skibu <andrey.skibunov2001@gmail.com>
2021-03-03 16:44:54 +0300

    fix lab1

---
+++ lab1/lab2.cpp
@@ -0,0 +1,65 @@
+//---------------------------------------------------------------------------
+//Скибунов андрей
+//23. Дана строка. Найти в строке длину самой длинной подстроки из одинаковых символов.
+#pragma hdrstop
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+#include<string.h>
+//---------------------------------------------------------------------------
+int foundSubString(char *string){
+  int max = 1;
+  int count = 1;
+  int len = strlen(string) - 1;
+  for(int i = 0; i < len; i++){
+    if(*(string + i) == *(string + i - 1)){
+      count++;
+    }
+    else{
+      if(count > max){
+        max = count;
+      }
+      count = 1;
+    }
+  }
+  return max;
+}
+
+#pragma argsused
+int main(int argc, char* argv[]){
+/*  char xx[] = "abcaab";
+  char* t;
+  int count = 0;
+  for(t = xx; *t; ++t)
+    if (*t == 'a')
+      count++;
+
+  t = xx;
+  while (*t){
+    if (*t == 'a')
+      count++;
+    t++;
+  }
+
+  printf("%d\n",count); */
+
+
+
+
+
+  char* string = new char[256];
+
+  puts("Enter string: ");
+  gets(string);
+
+
+  int maxCol = foundSubString(string);
+
+  printf("%d",maxCol);
+
+  delete []string;
+  getch();
+  return 0;
+}
+//---------------------------------------------------------------------------
+ 
